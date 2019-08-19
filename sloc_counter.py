6508b476a29d103f0e95754043736a1675edc31e
John Ingwersen <johni@iastate.edu>
2019-08-18 19:17:27 -0500

    Counts lines if not commented and not empty. Need to allow for any path and updating another file with the result.

--- sloc_counter.py
+++ sloc_counter.py
@@ -7,15 +7,24 @@ import os
 
 
 def count_lines_of_code():
-    my_dir = os.getcwd()
-    print(my_dir)
-    my_dir = os.path.dirname(os.getcwd())
-    print(my_dir)
+    my_dir = r'C:\Users\User\Desktop\Python\Personal_Fitness'
+    print("Looking in: " + my_dir + "\n\n")
 
+    sloc_count = 0
     for (dirpath, dirnames, filenames) in os.walk(my_dir):
         for name in filenames:
             if name.endswith('.py'):
-                print(name)
+                file_path = os.path.join(dirpath, name)
+                try:
+                    file = open(file_path, 'r')
+                    for line in file.readlines():
+                        line = line.replace(" ", "")
+                        if not line.startswith('#') and line != "":
+                            sloc_count += 1
+                except FileNotFoundError as e:
+                    print(e)
+                    raise
+    print(sloc_count)
 
 
 if __name__ == '__main__':
